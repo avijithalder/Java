e7eda950c42d1c6140ed1510768545384a5ee74f
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-07-01 15:30:56 +0600

    Input Demo.java

---
+++ Basic Java/Input Demo.java
@@ -0,0 +1,21 @@
+
+package Practice;
+
+import java.util.Scanner;
+
+public class InputDemo {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Any Number : ");
+
+        int number;
+        number = input.nextInt();
+
+        System.out.print("Number : " + number);
+
+    }
+
+}
