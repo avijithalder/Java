14ab40d9b36e5d66cbd5b82cd4b3e115da3b774a
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-07-30 23:17:27 +0600

    lood odd number.java

---
+++ Basic Java/lood odd number.java
@@ -0,0 +1,17 @@
+package Student_Detail;
+
+public class Student {
+
+	public static void main(String[] args) {
+			
+		for (int i = 1; i <= 99; i++) {
+
+			if (i / 2 != 0) {
+
+				i++;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
