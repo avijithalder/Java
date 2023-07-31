d782497c47e112478e6a820d4a8e1eadc6456a19
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-07-31 11:44:32 +0600

    Create 1.java

---
+++ Basic Java/w3resource Array Exercises/1.java
@@ -0,0 +1,19 @@
+package Hello;
+
+import java.util.Arrays;
+
+public class TryOn {
+
+	public static void main(String[] args) {
+		
+	int array1[]= {10,20,30,40,50,60,70};
+	String array2[]= {"g","a","c","b","d","e","f"};
+	
+	Arrays.sort(array1);
+	System.out.println(Arrays.toString(array1));
+	Arrays.sort(array2);
+	System.out.println(Arrays.toString(array2));
+	
+    
+}
+}
