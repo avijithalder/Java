29a357399e59874998876aa1c3a9b8512d407c86
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-07-31 11:54:54 +0600

    Create 2.java

---
+++ Basic Java/w3resource Array Exercises/2.java
@@ -0,0 +1,21 @@
+package Hello;
+
+import java.util.Arrays;
+
+public class TryOn {
+
+	public static void main(String[] args) {
+		
+		int array[] = {10,20,30};
+		int sum = 0;
+
+		for (int i : array) {
+		    sum =sum+i;
+		}
+		System.out.println("The sum is " + sum);
+		
+	
+	
+    
+}
+}
