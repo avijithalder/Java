a05bfbf4d9c226bb9fecb1d89741a814c9dbb062
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-11 21:42:09 +0600

    For each Loop.java

---
+++ Loop/For each Loop.java
@@ -0,0 +1,22 @@
+package Assingment1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] myArray = new int[5];
+		myArray[0] = 1;
+		myArray[1] = 2;
+		myArray[2] = 3;
+		myArray[3] = 4;
+		myArray[4] = 5;
+
+		//For Each Loop
+		for(int num:myArray)
+		{
+			System.out.println(num);
+		}
+
+	}
+
+}
