2944d1b48464d30b79d9d79ebf7171274d40e169
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-11 21:35:47 +0600

    While loop.java

---
+++ Loop/While loop.java
@@ -0,0 +1,23 @@
+package Assingment1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] myArray = new int[5];
+		myArray[0] = 1;
+		myArray[1] = 2;
+		myArray[2] = 3;
+		myArray[3] = 4;
+		myArray[4] = 5;
+
+		int i = 0;
+		while (i < 5) {
+			System.out.println(myArray[i]);
+			i++;
+
+		}
+
+	}
+
+}
