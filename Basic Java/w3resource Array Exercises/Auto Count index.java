eb54a1f440ffdec5d637676b7cb12cc60f2562a2
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-11 21:46:58 +0600

    Auto Count index.java

---
+++ Basic Java/w3resource Array Exercises/Auto Count index.java
@@ -0,0 +1,21 @@
+package Assingment1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] myArray = new int[5];
+		myArray[0] = 1;
+		myArray[1] = 2;
+		myArray[2] = 3;
+		myArray[3] = 4;
+		myArray[4] = 5;
+
+		for(int i=0;i<myArray.length;i++)
+		{
+			System.out.println(myArray[i]);
+		}
+
+	}
+
+}
