9ba1359aa0b399e0d3eabe604476f61c500c5628
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-14 23:20:53 +0600

    Create Ex1.java

---
+++ Strick pattern /Ex1.java
@@ -0,0 +1,19 @@
+package Star;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int array[][] = new int[7][10];
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
