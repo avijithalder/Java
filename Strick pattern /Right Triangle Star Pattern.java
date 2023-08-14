c4d03562cca9f6ff809439593cdf9ace734f85bb
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-14 23:40:56 +0600

    Right Triangle Star Pattern.java

---
+++ Strick pattern /Right Triangle Star Pattern.java
@@ -0,0 +1,22 @@
+package Star;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+	
+		int array[][] = new int[9][8];
+
+		for(int i=0;i<array.length;i++)
+		{
+			for(int j=0; j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
