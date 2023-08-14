24c36bd85b5b86d50770182a3e579f77859ad97f
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-15 00:31:06 +0600

    Create Hollow Inverted Right Triangle Star Pattern.java

---
+++ Strick pattern /Hollow Inverted Right Triangle Star Pattern.java
@@ -0,0 +1,20 @@
+package Star;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		int array[][] = new int[8][8];
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length-i; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
