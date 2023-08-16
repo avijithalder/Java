0629638106b31eaebfbf0c16e74e54a37ba2edd0
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-16 13:28:09 +0600

    Max number normal.java

---
+++ Basic Java/Max number normal.java
@@ -0,0 +1,17 @@
+package Star;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int num1 = 10, num2 = 20, max;
+
+//	max=(num1>num2)? num1:num2;
+		System.out.println("1st number :" + num1);
+		System.out.println("2nd number :" + num2);
+		max = num1 > num2 ? num1 : num2;
+		System.out.println("maximum number = " + max);
+
+	}
+
+}
