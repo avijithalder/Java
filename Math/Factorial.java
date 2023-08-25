864e1ffa7c4ceac38979f8c5754d5fdc1a2b503e
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-25 20:46:01 +0600

    Factorial.java

---
+++ Math/Factorial.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fact = 1;
+		for (int i = 1; i <= 6; i++) {
+			fact = fact * i;
+
+		}
+		System.out.print(fact);
+	}
+
+}
