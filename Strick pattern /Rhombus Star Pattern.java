114cce727283174f614bb902b7f1c337f523406a
Avijit Halder Joy <128460135+avijithalder@users.noreply.github.com>
2023-08-28 23:14:22 +0600

    Rhombus Star Pattern.java

---
+++ Strick pattern /Rhombus Star Pattern.java
@@ -0,0 +1,34 @@
+/*
+        *******
+       *******
+      *******
+     *******
+    *******
+   *******
+  *******
+ *******
+*******
+  */
+package Assessment2;
+
+import java.util.Arrays;
+
+class Main {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 10; i++) {
+
+			for (int j = 9; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j < 8; j++) {
+
+				System.out.print("*");
+			}
+
+			System.out.print("\n");
+
+		}
+
+	}
+}
